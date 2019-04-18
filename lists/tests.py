f286c8978add4a83f268d79d809afd642cf7d04b
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2019-04-18 14:50:57 -0700

    updated test.py to test if django used to correct template. Commented out old lines for referance on manual html testing

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-
 from django.urls import resolve
+from django.http import HttpRequest
 from lists.views import home_page
 
 # Create your tests here.
@@ -11,3 +11,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        """html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<!DOCTYPE html>\n<html lang="en"'))
+        self.assertIn('<title>To-Do Lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))"""
+        self.assertTemplateUsed(response, 'home.html')

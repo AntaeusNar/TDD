1e5b942925d1f80d3d8fc01014dd7466e78b9d25
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2019-04-22 10:50:20 -0700

    expanded home_page to add item to list

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 
 
 def home_page(request):
-    context = {'new_item_text': request.POST.get('item_text', '')}
+    # Todo: Don't save a black item for every request
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
+
+    context = {'new_item_text': item.text}
+    # Todo: Display multiple items in the table
     return render(request, 'home.html', context)

ea14619a58ec9c11f5326b0284ca2e06e40a82b9
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2019-04-23 14:25:38 -0700

    replaced time.sleep with a while True loop with a timeout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,9 +5,11 @@ each section should follow an human readable 'user story'
 
 from django.test import LiveServerTestCase
 from selenium import webdriver
-import unittest
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -18,10 +20,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def helper_check_for_row_in_table(self, row_text, table_id):
-        table = self.browser.find_element_by_id(table_id)
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def helper_wait_for_row_in_table(self, row_text, table_id):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id(table_id)
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -47,25 +57,22 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.helper_check_for_row_in_table('1: Buy peacock feathers', 'id_list_table')
+        self.helper_wait_for_row_in_table('1: Buy peacock feathers', 'id_list_table')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        self.helper_check_for_row_in_table('1: Buy peacock feathers', 'id_list_table')
-        self.helper_check_for_row_in_table('2: Use peacock feathers to make a fly', 'id_list_table')
+        self.helper_wait_for_row_in_table('1: Buy peacock feathers', 'id_list_table')
+        self.helper_wait_for_row_in_table('2: Use peacock feathers to make a fly', 'id_list_table')
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
-        # Todo: Cleanup after FT runs
         # todo: remove time.sleep
-        self.fail('Finish the test!')
+        self.fail('Finish the functional test!')
 
         # She visits that URL - her to-do list is still there.
 

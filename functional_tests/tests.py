50c362ae7bc3955a465b0e5c47ba9d3150b82c78
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2019-04-26 12:12:51 -0700

    Updated the black box:
    Added a helper for restarting the browser
    Added a new test for checking multiple users and urls

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -20,7 +20,11 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def helper_wait_for_row_in_table(self, row_text, table_id):
+    def helper_restart_browser(self):
+        self.tearDown()
+        self.setUp()
+
+    def helper_wait_for_row_in_table(self, row_text, table_id='id_list_table'):
         start_time = time.time()
         while True:
             try:
@@ -33,7 +37,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)
@@ -68,12 +72,48 @@ class NewVisitorTest(LiveServerTestCase):
         # The page updates again, and now shows both items on her list
         self.helper_wait_for_row_in_table('1: Buy peacock feathers', 'id_list_table')
         self.helper_wait_for_row_in_table('2: Use peacock feathers to make a fly', 'id_list_table')
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-        # todo: remove time.sleep
+
+        # Satisfied, she leaves her computer to go check her worms
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith wonders whether the site will remember her list.
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.helper_wait_for_row_in_table('1: Buy peacock feathers')
+
+        # She rushes back to the computer to see if there is a unique URL!
+        # Then she sees that the site has generated a unique URL for her.
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # Now a new user, Francis, comes along to the site.
+
+        # We use a new browser session to make sure the no information of Edith's is
+        # coming through from cookies etc
+        self.helper_restart_browser()
+
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entering a new item.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_key('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.helper_wait_for_row_in_table('1: Buy milk')
+
+        # Francis gets his own url
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Agian, there is no trace of Edith's list
+        page_text=self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
         self.fail('Finish the functional test!')
-
-        # She visits that URL - her to-do list is still there.
-
-        # Satisfied, she goes back to sleep
